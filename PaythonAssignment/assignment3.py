0336b8214372023a55eff777453748a8d977049b
FaziletKosure <faziletkosure1@gmail.com>
2020-07-07 20:00:34 +0200

    user input improvet

--- PaythonAssignment/assignment3.py
+++ PaythonAssignment/assignment3.py
@@ -1,4 +1,20 @@
-number = input("Enter the number : ")
+control_var = True
+
+while control_var:
+    number = input("Enter the number : ")
+
+    if '.' in number:
+        print("Please enter an integer number! ")
+        continue
+    elif number[0] == '-':
+        print(" Please enter a positive number! ")
+        continue
+    elif not number.isdigit():
+        print("Do not use any entries other than numeric values! ")
+        continue
+
+    else:
+        control_var = False
 n = len(number)
 print(n)
 sum_nth_pow = 0
